6ae70b5929e37765d5b3ca88c2d8f9199e29f84c
zukidono <zukidono@gmail.com>
2025-07-27 11:41:38 +0700

    added flags

--- ssh-copy-id-win.go
+++ ssh-copy-id-win.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("it is main")
+	port := flag.Int("p", 22, "port")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s: [user@]hostname\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Println("flag.NArg()", flag.NArg())
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Println("os.flag.Arg(1)", flag.Arg(0))
+	fmt.Println("port", *port)
 }
